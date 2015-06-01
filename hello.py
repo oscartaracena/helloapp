26b9175e19d3dca76fa5b18a93b62ec267621151
Oscar Taracena <oscar.taracena@gmail.com>
2015-06-01 14:58:55 -0700

    routes

--- hello.py
+++ hello.py
@@ -1,9 +1,19 @@
-from flask import Flask
+from flask import Flask, url_for
 app = Flask(__name__)
 
+@app.route('/profile/<username>')
+def show_user_profile(username):
+    return 'User: %s ' % username
+
+
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def show_url_for():
+    return url_for('show_user_profile', username = 'oscar')
+
+#@app.route('/post/<int:post_id>')
+#def show_post(post_id):
+    #return 'Post: %d ' %post_id
 
 if __name__ == '__main__':
+    app.debug = True
     app.run()
